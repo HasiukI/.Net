4a475b887ed10a62fa2b3c2e64794a5d4eac1d7b
HasiukI <136508423+HasiukI@users.noreply.github.com>
2023-11-12 23:20:42 +0200

    Read txt file in your PC
    read file finds your words

---
+++ ReadFile_inPC/BLL/FileWork.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class FileWork
+    {
+        public DirectoryInfo _maindirectory;
+        private List<string> _bunWords;
+        public int Progress { get; set; }
+        public int MaxProgress { get; set; }
+
+        public FileWork()
+        {
+            _maindirectory = Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).FullName).Parent;
+
+        }
+        async public Task FindWords(List<string> words)
+        {
+            _bunWords = words;
+            Progress = 0;
+            MaxProgress += 1;
+            string path = @"C:\Tetyana";
+            DirectoryInfo dir = new DirectoryInfo(path);
+            MaxProgress = dir.GetDirectories().Length;
+            await FindCountDirection(path);
+            await FindDirection(path);
+            await FileToChange();
+        }
+
+        #region ReadFileBanWord1
+        async public Task<List<string>> AsyncReadFileWord()
+        {
+
+            List<string> strings = new List<string>();
+
+            using (FileStream fileStream = new FileStream($@"{_maindirectory.FullName}\Words.txt", FileMode.Open, FileAccess.Read))
+            {
+                using (StreamReader sr = new StreamReader(fileStream, Encoding.UTF8))
+                {
+                    while (sr.Peek() > 0)
+                    {
+                        strings.Add(await sr.ReadLineAsync());
+
+                    }
+                }
+            }
+            return strings;
+        }
+        async public Task AddWordAsync(string word)
+        {
+            bool ok = true;
+            List<string> strings = await AsyncReadFileWord();
+            foreach (string s in strings)
+            {
+                if (s == word)
+                {
+                    ok = false;
+                    break;
+                }
+            }
+            using (FileStream fileStream = new FileStream($@"{_maindirectory.FullName}\Words.txt", FileMode.Append, FileAccess.Write))
+            {
+                using (StreamWriter sr = new StreamWriter(fileStream, Encoding.UTF8))
+                {
+
+                    if (ok)
+                        await sr.WriteLineAsync(word);
+                }
+            }
+        }
+        #endregion
+
+
+        #region FindFile2
+        async private Task FindCountDirection(string path)
+        {
+            DirectoryInfo dir = new DirectoryInfo(path);
+            DirectoryInfo[] subdirectories = dir.GetDirectories();
+            MaxProgress += dir.GetDirectories().Length;
+            foreach (DirectoryInfo subdirectory in subdirectories)
+            {
+                await FindCountDirection(subdirectory.FullName);
+            }
+        }
+        async private Task FindDirection(string path)
+        {
+            Progress += 1;
+            DirectoryInfo dir = new DirectoryInfo(path);
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+             
+                if (file.Extension == ".txt")
+                {
+                    if (await ReadFileAsync(file.FullName))
+                    {
+                        await Task.Run(() => file.CopyTo($@"{_maindirectory.FullName}\FindText\{file.Name}_copy.txt", true));
+                    }
+
+                }
+            }
+
+            DirectoryInfo[] subdirectories = dir.GetDirectories();
+            foreach (DirectoryInfo subdirectory in subdirectories)
+            {
+                await FindDirection(subdirectory.FullName);
+            }
+        }
+
+        async private Task<bool> ReadFileAsync(string path)
+        {
+
+            using (FileStream fileStream = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                using (StreamReader sr = new StreamReader(fileStream, Encoding.UTF8))
+                {
+                    string temp = "";
+                    while (sr.Peek() > 0)
+                    {
+                        temp = await sr.ReadLineAsync();
+                        foreach (string word in _bunWords)
+                        {
+                            if (temp.Contains(word))
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+        #endregion
+
+
+        #region ChangeFile3
+        async private Task FileToChange()
+        {
+
+            DirectoryInfo directoryInfo = new DirectoryInfo($@"{_maindirectory.FullName}\FindText");
+            FileInfo[] files = directoryInfo.GetFiles();
+
+            foreach (FileInfo file in files)
+            {
+                if (file.Extension == ".txt")
+                    await ChangeWord(file);
+            }
+        }
+        async private Task ChangeWord(FileInfo file)
+        {
+            string temp = "";
+            using (FileStream fileStream = new FileStream(file.FullName, FileMode.Open, FileAccess.Read))
+            {
+                using (StreamReader sr = new StreamReader(fileStream, Encoding.UTF8))
+                {
+
+                    while (sr.Peek() > 0)
+                    {
+                        temp += await sr.ReadLineAsync() + "\n";
+                    }
+                }
+            }
+
+            Dictionary<string, int> rez = new Dictionary<string,int>();
+            foreach (string word in _bunWords)
+            {
+                int count = temp.Split(new string[] { word }, StringSplitOptions.None).Length - 1;
+                rez.Add(word, count);
+                temp = temp.Replace(word, "*******");
+            }
+            using (FileStream fileStream = new FileStream($@"{_maindirectory.FullName}\FindFiles.txt", FileMode.Append, FileAccess.Write))
+            {
+                using (StreamWriter sr = new StreamWriter(fileStream, Encoding.UTF8))
+                {
+                    string write = $"Файл: {file.Name} | ";
+                    foreach (var keys in rez)
+                    {
+                        write += $"Найдено <{keys.Key}>   раз: {keys.Value} | ";
+                    }
+                    await sr.WriteLineAsync(write);
+                }
+            }
+
+            using (FileStream fileStream = new FileStream(file.FullName, FileMode.Open, FileAccess.Write))
+            {
+                using (StreamWriter sr = new StreamWriter(fileStream, Encoding.UTF8))
+                {
+                    await sr.WriteLineAsync(temp);
+
+                }
+            }
+        }
+        #endregion
+
+        public void OpenFile(string name)
+        {
+            Process.Start($@"{_maindirectory.FullName}\{name}.txt");
+        }
+        public string getPhoto()
+        {
+            string str = "";
+            foreach(DirectoryInfo directory in _maindirectory.GetDirectories())
+            {
+                if(directory.Name == "Exam_Sys")
+                {
+                    foreach(DirectoryInfo dir in directory.GetDirectories())
+                    {
+                        if (dir.Name == "images")
+                        {
+                            foreach(FileInfo file in dir.GetFiles())
+                            {
+                                if (file.Name == "car.png")
+                                {
+                                    return file.FullName;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return "";
+        }
+
+    }
+}
---
+++ ReadFile_inPC/Exam_Sys/MainWindow.xaml.cs
@@ -0,0 +1,200 @@
+﻿using BLL;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Exam_Sys
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        FileWork read_write;
+        Thread thread_file, thread_game;
+        Ellipse el;
+        Image imagecar;
+
+        int rez = 0;
+        int pos =0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            el = new Ellipse() { Width = 10, Height = 10, Fill = Brushes.White };
+            el.MouseLeftButtonUp += El_MouseLeftButtonUp;
+            read_write = new FileWork();
+            GamePause.Children.Add(el);
+
+            imagecar = new Image() {  Height = 40};
+            imagecar.Source = new BitmapImage(new Uri(read_write.getPhoto()));
+            Canvas.SetBottom(imagecar, 0);
+            Canvas.SetLeft(imagecar, 0);
+            Progress.Children.Add(imagecar);
+        }
+        #region FileWorks
+        async private void addWordMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (!String.IsNullOrEmpty(Word_TBox.Text) && !String.IsNullOrWhiteSpace(Word_TBox.Text))
+            {
+                if (Word_TBox.Text.Length < 20 && Word_TBox.Text.Length >= 3)
+                {
+                    if (Word_TBox.Text.IndexOfAny(new char[] { ' ', '.', '/', '*', ',', '[', ']', '<', '>', '(', ')', ':', ';', '?', '!', '}', '{' }) == -1)
+                    {
+                        await read_write.AddWordAsync(Word_TBox.Text);
+                        WordsInFile_LB.ItemsSource = await read_write.AsyncReadFileWord();
+                        Word_TBox.Text = "";
+                    }
+                }
+            }
+        }
+        private void WordsInFile_LB_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (SelectedWords_LB.Items.IndexOf(WordsInFile_LB.SelectedItem) == -1)
+            {
+                SelectedWords_LB.Items.Add(WordsInFile_LB.SelectedItem);
+            }
+        }
+
+        private void SelectedWords_LB_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            SelectedWords_LB.Items.Remove(SelectedWords_LB.SelectedItem);
+        }
+
+        private void OpenFileMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if ((sender as Border).Name == "find_bord")
+                read_write.OpenFile("FindFiles");
+            if ((sender as Border).Name == "words_bord")
+                read_write.OpenFile("Words");
+
+        }
+        #endregion
+        #region Window
+        async private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            WordsInFile_LB.ItemsSource = await read_write.AsyncReadFileWord();
+        }
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if(thread_game!=null)
+                thread_game.Abort();
+            if (thread_file != null)
+                thread_file.Abort();
+        }
+        #endregion
+        #region GameWork
+        private void updateGame()
+        {
+            Stopwatch timer_spawn = new Stopwatch();
+            timer_spawn.Start();
+            while (true)
+            {
+
+                if (timer_spawn.Elapsed.TotalSeconds > 1)
+                {
+                    Task.Run(() => createElipse(Randomizer.GetPos(Convert.ToInt32(GamePause.ActualHeight), Convert.ToInt32(GamePause.ActualWidth))));
+                    timer_spawn.Restart();
+                }
+
+            }
+        }
+        private void createElipse(int[] pos)
+        {
+
+            Dispatcher.Invoke(() => Canvas.SetLeft(el, pos[0]));
+            Dispatcher.Invoke(() => Canvas.SetTop(el, pos[1]));
+
+        }
+
+        private void El_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            rez++;
+            rez_tb.Text = $"Ваші бали: {rez}";
+        }
+        private void RunCur()
+        {
+            int run = 0;
+            Dispatcher.Invoke(() => run = Convert.ToInt32(Progress.ActualWidth) * read_write.Progress / read_write.MaxProgress);
+            Dispatcher.Invoke(() => Canvas.SetLeft(imagecar, run));
+        }
+        #endregion
+
+
+        async private void FindStart_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            List<string> words = new List<string>();
+            thread_file = new Thread(UpdateValue);
+            thread_file.Start();
+            thread_game = new Thread(updateGame);
+            thread_game.Start();
+            foreach (string word in SelectedWords_LB.Items)
+            {
+                words.Add(word);
+            }
+
+            await read_write.FindWords(words);
+            
+        }
+
+        #region Decor
+        private void Border_MouseEnter(object sender, MouseEventArgs e)
+        {
+            (sender as Border).Background = Brushes.LightGray;
+        }
+
+        private void Border_MouseLeave(object sender, MouseEventArgs e)
+        {
+            (sender as Border).Background = Brushes.White;
+        }
+
+        #endregion
+
+      
+
+        private void UpdateValue()
+        {
+            while (true)
+            {
+                if (Dispatcher.Invoke(() => read_write.Progress) >= Dispatcher.Invoke(() => read_write.MaxProgress)-20)
+                {
+                    thread_file.Abort();
+                    thread_game.Abort();
+                }
+                Task.Run(() => RunCur());
+            }
+
+        }
+       
+       
+       
+
+       
+    }
+    static class Randomizer
+    {
+        static private Random random = new Random();
+        public static double GetTimeSpawn()
+        {
+            return random.Next(0, 3);
+        }
+        public static int[] GetPos(int h, int w)
+        {
+            return new int[] { random.Next(0, h ), random.Next(0, w) };
+        }
+    }
+}
