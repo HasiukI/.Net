b668871ec35920b9d2206dcd5593b68ca57f66dc
HasiukI <136508423+HasiukI@users.noreply.github.com>
2023-12-05 00:01:21 +0200

    create animation with pagw

--- LocalMessenger/Client_LocalMessenger/Client_LocalMessenger/MainWindow.xaml.cs
+++ LocalMessenger/Client_LocalMessenger/Client_LocalMessenger/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Interop;
 
+
 namespace Client_LocalMessenger
 {
     /// <summary>
@@ -25,40 +26,50 @@ namespace Client_LocalMessenger
     {
         string address = string.Empty;
         int port = 0;
-        
+
+
         public MainWindow()
         {
             InitializeComponent();
-        }
+			WaitForServerName();
+			
+		}
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                UdpClient receiver = new UdpClient(8888);
-            
-                IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 0);
-                byte[] datagram = receiver.Receive(ref endPoint);
-                string[] msg = Encoding.UTF8.GetString(datagram).Split(' '); 
+		private void WaitForServerName()
+		{
+			try
+			{
+				UdpClient receiver = new UdpClient(8888);
 
-                receiver.Close();
+				IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 0);
+				byte[] datagram = receiver.Receive(ref endPoint);
+				string[] msg = Encoding.UTF8.GetString(datagram).Split(' ');
 
-                address = msg[0];
-                port = int.Parse(msg[1]);
-                tb.Text=string.Format("Addres: {0}, Port: {1}",address,port);
-            }
-            catch (Exception ex)
-            {
-                if(ex.Message== "Only one usage of each socket address (protocol/network address/port) is normally permitted.")
-                {
-                    MessageBox.Show("Зачекайте будь ласка пробує підєднатись інший користувач");
-                }
-                else
-                {
-                    MessageBox.Show(ex.Message);
-                }
-               
-            }
-        }
-    }
+				receiver.Close();
+
+				address = msg[0];
+				port = int.Parse(msg[1]);
+
+				if (!string.IsNullOrEmpty(address) && port != 0)
+				{
+					btGoLogin.Visibility = Visibility.Visible;
+				}
+
+			}
+			catch (Exception ex)
+			{
+				if (ex.Message == "Only one usage of each socket address (protocol/network address/port) is normally permitted.")
+				{
+					MessageBox.Show("Зачекайте будь ласка пробує підєднатись інший користувач");
+				}
+				else
+				{
+					MessageBox.Show(ex.Message);
+				}
+
+			}
+		}
+
+
+	}
 }
